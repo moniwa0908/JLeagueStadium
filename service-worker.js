a4577c38dbb92b60b947001b5480b495332fe059
moniwa0908 <ko6subi4syo@yahoo.co.jp>
2026-08-13 18:21:07 +0900

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-const CACHE='j-stadium-v23';
-const FILES=['./','./index.html','./firebase-config.js?v=23','./stadium-details.js?v=23','./firebase-shared.js?v=23','./app.js?v=23','./manifest.json?v=23','./apple-touch-icon.png?v=23','./icon-192.png?v=23','./icon-512.png?v=23'];
+const CACHE='j-stadium-v24';
+const FILES=['./','./index.html','./firebase-config.js?v=24','./stadium-details.js?v=24','./firebase-shared.js?v=24','./app.js?v=24','./manifest.json?v=23','./apple-touch-icon.png?v=23','./icon-192.png?v=23','./icon-512.png?v=23'];
 self.addEventListener('install',event=>{
   self.skipWaiting();
   event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(FILES)));
