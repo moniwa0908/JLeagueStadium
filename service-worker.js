18cfe50dfbaa3f763cfc0442cbd0673644bbd2c8
moniwa0908 <ko6subi4syo@yahoo.co.jp>
2026-08-13 12:15:39 +0900

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-const CACHE='j-stadium-v10';
-const FILES=['./','./index.html','./app.js?v=10','./manifest.json'];
+const CACHE='j-stadium-v11';
+const FILES=['./','./index.html','./app.js?v=11','./manifest.json'];
 self.addEventListener('install',event=>{
   self.skipWaiting();
   event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(FILES)));
